eb94033e0cc41310b6939900c736931cbf3df343
Matteo <info@matteosimone.it>
2021-08-31 14:03:17 +0200

    feat: add nav bar menu

--- inc/inc.header.php
+++ inc/inc.header.php
@@ -5,6 +5,6 @@
 <link href="./node_modules/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
 <link href="./node_modules/gentelella/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
 <link href="./node_modules/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
-<link href="./node_modules/gentelella/build/css/custom.css" rel="stylesheet">
+<link href="./node_modules/gentelella/build/css/custom.min.css" rel="stylesheet">
 <link href="./node_modules/animate.css/animate.min.css" rel="stylesheet">
 <link href="../css/custom.css" rel="stylesheet">
---
+++ inc/inc.navbar.php
@@ -0,0 +1,11 @@
+<nav class=" navbar-nav">
+    <ul class=" navbar-right">
+        <li class="nav-item dropdown open" style="padding-left: 15px;">
+
+            <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="javascript:;"> Profile</a>
+                <a class="dropdown-item" href="login.html"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
+            </div>
+        </li>
+    </ul>
+</nav>
